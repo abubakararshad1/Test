569bc81a470b58c0311ae9217f07de1b7d636f1e
Abubakar.Arshad <abubakar.arshad1@gmail.com>
2025-02-21 09:43:07 +0500

    testing

--- test_google_title.py
+++ test_google_title.py
@@ -12,4 +12,4 @@ time.sleep(5)
 driver.find_element(By.XPATH, "//input[@id='password']").send_keys("secret_sauce")
 time.sleep(5)
 
-#this is testing to check the code 1 is working perfect or not
+#abubakar
